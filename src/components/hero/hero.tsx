fb7620993b5bc3bed24748c4eb0aa4c076592188
moatasim <mo32000a@gmail.com>
2024-09-11 09:51:41 +0300

    hero 2

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -3,6 +3,8 @@ import styles from "./hero.module.css"
 import Image from 'next/image'
 import hero from "../../public/assets/images/hero.png"
 import user1 from "../../public/assets/images/profile-1.jpeg"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 const Hero = () => {
   return (
     <section className={styles.hero}>
@@ -11,8 +13,27 @@ const Hero = () => {
                 <Image src={hero} alt='Black male smiling' loading='lazy'></Image>
             </div>
             <div className={styles.card}>
-                <Image src={user1} alt='Black male smiling user image' loading='lazy'></Image>
+                <div className={styles.cardImg}>
+                    <Image src={user1} alt='Black male smiling user image' loading='lazy'></Image>
+                </div>
+                <div className={styles.cardText}>
+                    <h3>James Bruno</h3>
+                    <span>Digital Marketer</span>
+                </div>
             </div>
+        </div>
+        <div className={styles.text}>
+            <h1>We Help You Build & Grow Online Business</h1>
+            <form>
+                <div className={styles.searchBox}>
+                    <input type="search" placeholder='Tell us what you need to build...'/>
+                    <button>
+                        <span>
+                            <FontAwesomeIcon icon={faSearch}></FontAwesomeIcon>
+                        </span>
+                    </button>
+                </div>
+            </form>
         </div>
     </section>
   )
