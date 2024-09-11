7a3abb05fddcf51b53731b6124271aa0367d0fc7
moatasim <mo32000a@gmail.com>
2024-09-11 14:10:23 +0300

    clients 2 (made it responsive)

--- src/components/clients/clients.tsx
+++ src/components/clients/clients.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import logo1 from "../../public/assets/images/media-24.jpg"
 import logo2 from "../../public/assets/images/westin-1.jpg"
 import logo3 from "../../public/assets/images/icom.png"
-import logo4 from "../../public/assets/images/smart.png"
+import logo4 from "../../public/assets/images/smart.jpg"
 import logo5 from "../../public/assets/images/one-line.jpg"
 
 
@@ -13,21 +13,11 @@ const Clients = () => {
     <section className={styles.clients}>
         <p>Companies we've helped build</p>
         <div className={styles.logos}>
-            <div className={styles.img}>
                 <Image src={logo1} alt='media 24 logo'></Image>
-            </div>
-            <div className={styles.img}>
-                <Image src={logo3} alt='icom logo'></Image>
-            </div>
-            <div className={styles.img}>
                 <Image src={logo2} alt='westin 1 logo'></Image>
-            </div>
-            <div className={styles.img}>
-                <Image src={logo4} alt='smart logo'></Image>
-            </div>
-            <div className={styles.img}>
                 <Image src={logo5} alt='one line logo'></Image>
-            </div>
+                <Image src={logo4} alt='smart logo'></Image>
+                <Image src={logo3} alt='icom logo'></Image>
         </div>
     </section>
   )
